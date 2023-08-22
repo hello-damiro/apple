345fa2037b63e8ffad4b5cb630a0f3edbd4e508a
damiro <hello.damiro@gmail.com>
2023-08-22 21:54:37 +0800

    sams starting point

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -8,82 +8,73 @@ import { movieData } from '@/data/movies';
 import Picture from './Picture';
 
 const movies = movieData;
+const movieWidth = 400;
+const twWidth = 'w-[400px]';
 
 export default function Carousel() {
-  const width = 400;
-  let [ref] = useMeasure();
   let [count, setCount] = useState(0);
   let prev = usePrevious(count);
-  let direction = count > prev ? 1 : -1;
-
-  let variants = {
-    enter: ({ direction, width }) => ({ x: direction * width }),
-    center: { x: 0, transition: { type: 'spring', stiffness: 30, duration: 0.4 } },
-    exit: ({ direction, width }) => ({
-      x: direction * -width,
-      transition: { type: 'spring', stiffness: 30, duration: 0.4 },
-    }),
-  };
 
   const handleLeftButtonClick = () => {
     setCount(count - 1 < 0 ? movies.length - 1 : count - 1);
+    console.log(count);
   };
 
   const handleRightButtonClick = () => {
     setCount(count + 1 >= movies.length ? 0 : count + 1);
+    console.log(count);
   };
 
-  const getMoviesInRange = () => {
-    const range = 2;
-    const totalMovies = movies.length;
-    const startIndex = (count - range + totalMovies) % totalMovies;
-    const endIndex = (count + range + totalMovies) % totalMovies;
+  function rearrangeMovies(movies) {
+    const middleIndex = Math.floor(movies.length / 2);
+    const newArray = [];
 
-    const idsInRange = [];
+    for (let i = middleIndex + 1; i < movies.length; i++) newArray.push(movies[i]); // Add movies after the middleIndex to the new array
+    newArray.push(movies[0]); // Add movie[0] at middleIndex to the new array
+    for (let i = 1; i <= middleIndex; i++) newArray.push(movies[i]); // Add movies before the middleIndex to the new array
 
-    if (endIndex >= startIndex) {
-      for (let i = startIndex; i <= endIndex; i++) idsInRange.push(i);
-    } else {
-      for (let i = startIndex; i < totalMovies; i++) idsInRange.push(i);
-      for (let i = 0; i <= endIndex; i++) idsInRange.push(i);
-    }
-
-    const moviesInRange = idsInRange.map((id) => movies[id]);
-
-    return moviesInRange;
-  };
-
-  const moviesInRange = getMoviesInRange();
+    return newArray;
+  }
 
   return (
-    <div className='relative overflow-hidden'>
-      <div ref={ref} className='relative mx-auto w-full h-full py-4 no-scrollbar'>
-        <div className='flex flex-nowrap pl-6 w-full h-auto justify-center'>
-          {/* CARDS */}
-          {moviesInRange.map((movie) => (
-            <div key={movie.id} className='scale-100 hover:scale-[1.02] transition-all duration-700 pr-6'>
-              <div
-                className={cn(
-                  'overflow-hidden rounded-2xl shadow-gray-md shadow-sm hover:shadow-lg transition-all duration-700',
-                  'bg-red-200 w-[400px] aspect-[16/9]' // Card dimensions
-                )}
-              >
-                <Picture src={movie.src} alt={movie.title} style={{ width: '100%', height: '100%' }} />
-              </div>
-            </div>
+    <div className='relative py-4 overflow-hidden'>
+      {/* CAROUSEL */}
+      <div className={cn('flex mx-auto aspect-video', twWidth)}>
+        <div className={'relative w-full'}>
+          {movies.map((movie) => (
+            <Movie key={movie.id} id={movie.id} src={movie.src} title={movie.title} />
           ))}
         </div>
       </div>
 
       {/* BUTTONS */}
-      <div className='absolute w-full h-auto flex justify-between px-6 top-1/2 -translate-y-1/2 p-2'>
-        <button onClick={handleLeftButtonClick} className=' rounded-full bg-brand-black'>
-          <MdChevronLeft className='h-10 w-10 m-2 flex text-brand-white' />
-        </button>
-        <button onClick={handleRightButtonClick} className='rounded-full bg-brand-black'>
-          <MdChevronRight className='h-10 w-10 m-2 flex text-brand-white' />
-        </button>
-      </div>
+      <button
+        onClick={handleLeftButtonClick}
+        className='absolute top-1/2 -translate-y-1/2 rounded-full bg-brand-black ml-6'
+      >
+        <MdChevronLeft className='h-10 w-10 m-2 flex text-brand-white' />
+      </button>
+      <button
+        onClick={handleRightButtonClick}
+        className='absolute top-1/2 -translate-y-1/2 right-0 rounded-full bg-brand-black mr-6'
+      >
+        <MdChevronRight className='h-10 w-10 m-2 flex text-brand-white' />
+      </button>
     </div>
+  );
+}
+
+function Movie({ src, title, id }) {
+  return (
+    <motion.span
+      style={{ x: movieWidth * id }}
+      className={cn(
+        'absolute inset-0 flex justify-center',
+        'scale-100 hover:scale-[1.02] transition-all duration-700',
+        'overflow-hidden rounded-2xl shadow-gray-md shadow-sm hover:shadow-lg transition-all duration-700'
+      )}
+    >
+      <Picture src={src} alt={title} style={{ width: '100%', height: '100%' }} />
+    </motion.span>
   );
 }
